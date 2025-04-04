f11155977bc39658bf6df39a800dda27e8199216
Nir Kogman <nirkog@gmail.com>
2025-04-04 22:05:13 +0000

    Better production stuff

--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
+
+change_socket_permissions() {
+	sleep 2
+	chmod 777 filmby.sock
+}
+
 if [ "$#" -gt 0 ]; then
 	if [ "$1" = "-i" ]; then
 		export IGNORE_CACHE=1
 	fi
 fi
 
-gunicorn --pythonpath src -b 0.0.0.0:443 server.wsgi:app --certfile /root/ssl/cert.pem --key /root/ssl/key.pem
+change_socket_permissions &
+gunicorn --bind unix:filmby.sock -m 007 --pythonpath src server.wsgi:app --log-level DEBUG
+# fg
+# while true
+# do
+# 	sleep 10
+# done
+# gunicorn --log-level DEBUG --pythonpath src -b 0.0.0.0:443 server.wsgi:app --certfile /root/ssl/cert.pem --key /root/ssl/key.pem --ssl-version TLS_SERVER
+# gunicorn --pythonpath src -b 0.0.0.0:443 server.wsgi:app --key /root/ssl/key.pem
